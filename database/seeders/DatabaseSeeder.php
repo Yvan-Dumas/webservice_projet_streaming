f59ff5479df62993f6e6c1bbe9905692fb1f3bff
Yvan Dumas <yvandumas05@gmail.com>
2026-06-05 22:16:45 +0200

    Seeder pour les artistes, utilisateur de test, styles, albums et musiques

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,11 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Album;
+use App\Models\Artiste;
+use App\Models\Style;
+use App\Models\Musique;
+
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
@@ -15,11 +20,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // test utilisateur
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@test.com',
+            'password' => bcrypt('password'),
         ]);
+
+        // Création des styles
+        $styles = ['Rock', 'Rap', 'Electro', 'Jazz', 'Pop'];
+        foreach ($styles as $styleNom) {
+            Style::create(['nom_style' => $styleNom]);
+        }
+
+        $tousLesStyles = Style::all();
+
+        // Création de 3 artistes
+        Artiste::factory(3)->create()->each(
+            function ($artiste) use ($tousLesStyles) {
+            // 2 albums par artiste
+            Album::factory(2)->create(['artiste_id' => $artiste->id])->each(
+                function ($album) use ($tousLesStyles) {
+                    // On génère 8 musiques associées à chaque album
+                    for ($i = 1; $i <= 8; $i++) {
+                        $musique=Musique::factory()->create([
+                            'album_id' => $album->id,
+                            'numero_musique' => $i,
+                        ]);
+
+                        $stylesAleatoires = $tousLesStyles->random(rand(1, 2));
+                        $musique->styles()->attach($stylesAleatoires);
+                    }
+                }
+            );
+        });
     }
 }
