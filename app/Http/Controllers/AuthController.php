6d41b699a613d7560f7d42a4ee3995ad9d08f3cf
Yvan Dumas <yvandumas05@gmail.com>
2026-06-05 13:35:45 +0200

    méthodes login et register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,4 +35,41 @@ class AuthController extends Controller
             'user' => $user
         ], 201);
     }
+
+    public function login(Request $request)
+    {
+        // Validation des champs requis
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Recherche de l'utilisateur en BD via l'email fourni
+        $user = User::where('email', $request->email)->first();
+
+        // Vérification de l'existence de l'utilisateur et du mot de passe
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Identifiants de connexion incorrects.'
+            ], 401);
+        }
+
+        // Si tout est bon, on génère un nouveau token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        // On renvoie le token au client
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Déconnexion réussie. Le token a été supprimé.'
+        ]);
+    }
 }
