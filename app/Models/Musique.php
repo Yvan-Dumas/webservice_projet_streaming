574e6c743ac72b78d0277958b70df65b039296e9
Yvan Dumas <yvandumas05@gmail.com>
2026-06-05 22:18:26 +0200

    Ajout HasFactory pour la création des factories

--- app/Models/Musique.php
+++ app/Models/Musique.php
@@ -3,7 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Musique extends Model
 {
+    use HasFactory;
+
+    public function styles()
+    {
+        return $this->belongsToMany(Style::class);
+    }
 }
