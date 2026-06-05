6db0d80e94ae357704c2c135180bd78a43ad8e08
Yvan Dumas <yvandumas05@gmail.com>
2026-06-05 13:26:15 +0200

    Routes d'authentification

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
+// --- Routes publiques
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+// --- Routes privées
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/user/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
