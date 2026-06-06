b25645432c912c4b121b1fdf711aa6d800ae1fb1
Yvan Dumas <yvandumas05@gmail.com>
2026-06-06 22:34:17 +0200

    création de musique gratuites

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,21 +38,28 @@ class DatabaseSeeder extends Seeder
         // Création de 3 artistes
         Artiste::factory(3)->create()->each(
             function ($artiste) use ($tousLesStyles) {
-            // 2 albums par artiste
-            Album::factory(2)->create(['artiste_id' => $artiste->id])->each(
-                function ($album) use ($tousLesStyles) {
+                // 2 albums par artiste
+                Album::factory(2)->create(['artiste_id' => $artiste->id])->each(
+                    function ($album) use ($tousLesStyles) {
                     // On génère 8 musiques associées à chaque album
                     for ($i = 1; $i <= 8; $i++) {
-                        $musique=Musique::factory()->create([
+                        $attributs = [
                             'album_id' => $album->id,
                             'numero_musique' => $i,
-                        ]);
+                        ];
+
+                        if ($i === 1) {
+                            $attributs['prix'] = 0;
+                        }
+
+                        $musique = Musique::factory()->create($attributs);
 
                         $stylesAleatoires = $tousLesStyles->random(rand(1, 2));
                         $musique->styles()->attach($stylesAleatoires);
                     }
                 }
-            );
-        });
+                );
+            }
+        );
     }
 }
