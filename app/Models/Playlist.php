9faffb59943f13e157b5400a12f33dbd96a171bb
Yvan Dumas <yvandumas05@gmail.com>
2026-06-06 22:36:22 +0200

    méthode pour récupérer les musiques associées à une musique

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Playlist extends Model
 {
-    //
+    protected $fillable = [
+        'nom_playlist',
+        'user_id'
+    ];
+
+    public function musiques(){
+        return $this->belongsToMany(Musique::class);
+    }
 }
